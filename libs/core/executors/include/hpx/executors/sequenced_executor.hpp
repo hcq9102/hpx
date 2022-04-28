6f67c50cc3828b934d8347e28c76ddea18b94bb4
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2022-04-28 14:01:56 -0500

    Update sequenced_executor.hpp

--- libs/core/executors/include/hpx/executors/sequenced_executor.hpp
+++ libs/core/executors/include/hpx/executors/sequenced_executor.hpp
@@ -136,6 +136,116 @@ namespace hpx { namespace execution {
         }
         /// \endcond
     };
+    
+    ///////////////////////////////////////////////////////////////////////////
+    /// A \a unsequential_executor creates groups of unsequential execution 
+    /// agents which execute in the calling thread. 
+    ///
+    struct unsequenced_executor
+    {
+        /// \cond NOINTERNAL
+        bool operator==(unsequenced_executor const& /*rhs*/) const noexcept
+        {
+            return true;
+        }
+
+        bool operator!=(unsequenced_executor const& /*rhs*/) const noexcept
+        {
+            return false;
+        }
+
+        unsequenced_executor const& context() const noexcept
+        {
+            return *this;
+        }
+        /// \endcond
+
+        /// \cond NOINTERNAL
+        typedef unsequenced_execution_tag execution_category;
+
+        // OneWayExecutor interface
+        template <typename F, typename... Ts>
+        static
+            typename hpx::util::detail::invoke_deferred_result<F, Ts...>::type
+            sync_execute(F&& f, Ts&&... ts)
+        {
+            return hpx::detail::sync_launch_policy_dispatch<
+                launch::sync_policy>::call(launch::sync, HPX_FORWARD(F, f),
+                HPX_FORWARD(Ts, ts)...);
+        }
+
+        // TwoWayExecutor interface
+        template <typename F, typename... Ts>
+        static hpx::future<
+            typename hpx::util::detail::invoke_deferred_result<F, Ts...>::type>
+        async_execute(F&& f, Ts&&... ts)
+        {
+            return hpx::detail::async_launch_policy_dispatch<
+                launch::deferred_policy>::call(launch::deferred,
+                HPX_FORWARD(F, f), HPX_FORWARD(Ts, ts)...);
+        }
+
+        // NonBlockingOneWayExecutor (adapted) interface
+        template <typename F, typename... Ts>
+        static void post(F&& f, Ts&&... ts)
+        {
+            sync_execute(HPX_FORWARD(F, f), HPX_FORWARD(Ts, ts)...);
+        }
+
+        // BulkTwoWayExecutor interface
+        template <typename F, typename S, typename... Ts>
+        static std::vector<hpx::future<typename parallel::execution::detail::
+                bulk_function_result<F, S, Ts...>::type>>
+        bulk_async_execute(F&& f, S const& shape, Ts&&... ts)
+        {
+            typedef
+                typename parallel::execution::detail::bulk_function_result<F, S,
+                    Ts...>::type result_type;
+            std::vector<hpx::future<result_type>> results;
+
+            try
+            {
+                for (auto const& elem : shape)
+                {
+                    results.push_back(async_execute(f, elem, ts...));
+                }
+            }
+            catch (std::bad_alloc const& ba)
+            {
+                throw ba;
+            }
+            catch (...)
+            {
+                throw exception_list(std::current_exception());
+            }
+
+            return results;
+        }
+
+        template <typename F, typename S, typename... Ts>
+        static typename parallel::execution::detail::bulk_execute_result<F, S,
+            Ts...>::type
+        bulk_sync_execute(F&& f, S const& shape, Ts&&... ts)
+        {
+            return hpx::unwrap(bulk_async_execute(
+                HPX_FORWARD(F, f), shape, HPX_FORWARD(Ts, ts)...));
+        }
+
+        template <typename Parameters>
+        std::size_t processing_units_count(Parameters&&) const
+        {
+            return 1;
+        }
+
+    private:
+        friend class hpx::serialization::access;
+
+        template <typename Archive>
+        void serialize(Archive& /* ar */, const unsigned int /* version */)
+        {
+        }
+        /// \endcond
+    }; 
 }}    // namespace hpx::execution
 
 namespace hpx { namespace parallel { namespace execution {
